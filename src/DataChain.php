66e646f4861205da3200c494019af68c614579ca
klc <m_kilic@outlook.com.tr>
2021-08-29 16:44:47 +0300

    init
    initial commit

---
+++ src/DataChain.php
@@ -0,0 +1,79 @@
+<?php
+
+abstract class DataChain
+{
+    /** @var $next DataChain */
+    protected $next;
+
+    /** @var $preview DataChain */
+    protected $preview;
+
+    /**
+     * @param array $params
+     * @return mixed
+     */
+    abstract protected function handle(array $params);
+
+    /**
+     * @param array $params
+     * @param mixed $result
+     * @return mixed
+     */
+    protected function terminate(array $params, $result)
+    {
+        return false;
+    }
+
+    /**
+     * @param DataChain $chain
+     * @return DataChain
+     */
+    public function next(DataChain $chain)
+    {
+        $this->next = $chain;
+        $chain->preview = $this;
+
+        return $chain;
+    }
+
+    /**
+     * @param array $params
+     * @return false|mixed|void
+     */
+    public function run(array $params = [])
+    {
+        $result = $this->handle($params);
+
+        if ($result !== false) {
+            if ($this->preview) {
+
+                return $this->preview->handleRecover($params, $result);
+            }
+
+            return $result;
+        }
+
+        if ($this->next) {
+
+            return $this->next->run($params);
+        }
+    }
+
+    /**
+     * @param $params
+     * @param $result
+     * @return false|mixed
+     */
+    protected function handleRecover($params, $result)
+    {
+        $result = $this->terminate($params, $result);
+
+        if ($result !== false && $this->preview) {
+
+            return $this->preview->handleRecover($params, $result);
+        }
+
+        return $result;
+    }
+
+}
